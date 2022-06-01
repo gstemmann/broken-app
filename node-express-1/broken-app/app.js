577b427493833ef6f09d7f0d151e3950c53697eb
george <george.stemmann@gmail.com>
2022-06-01 16:09:30 -0700

    added api call proper

--- node-express-1/broken-app/app.js
+++ node-express-1/broken-app/app.js
@@ -1,18 +1,58 @@
 const express = require('express');
-let axios = require('axios');
-var app = express();
+const app = express();
+const ExpressError = require("./expressError")
+const axios = require('axios');
 
-app.post('/', function(req, res, next) {
-  try {
-    let results = req.body.developers.map(async d => {
-      return await axios.get(`https://api.github.com/users/${d}`);
-    });
-    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
+// app.use(express.json)
+const base_url = `https://api.github.com/users/blue`
 
-    return res.send(JSON.stringify(out));
-  } catch {
-    next(err);
-  }
+
+
+
+app.get('/', async function(req, res, next) {
+
+    try {
+        const result = await axios.get(base_url)
+        console.log(result.data.name)
+        console.log(result.data.bio)
+        let outPut = result.data.bio
+        // res.send(result.data.bio)
+        res.send(JSON.stringify(outPut))
+    } catch (err) {
+        return next(err)
+    }
+    
+
+    // try {
+    //     const results = req.body.developer.map(async d=> {
+    //         return await axios.get(`base_url`)
+    //     })
+    //     let outPut = results.map(r=> ({name: r.data.name, bio: r.data.bio}))
+    // return red.send(JSON.stringify(outPut))
+    // } catch (err) {
+    //     return next(err)
+    // }
+})
+
+
+
+// app.post('/', function(req, res) {
+
+//   try {
+//     let results = req.body.developers.map(async d => {
+//       return await axios.get(`https://api.github.com/users/${d}`);
+//     });
+//     let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
+
+//     return res.send(JSON.stringify(out));
+//   } catch {
+//     next(err);
+//   }
+
+// });
+
+
+
+app.listen(3000, () => {
+    console.log('running on port 3000')
 });
-
-app.listen(3000);
--- node-express-1/broken-app/issues.md
+++ node-express-1/broken-app/issues.md
@@ -1,1 +1,5 @@
 # Broken App Issues
+ - var and let in first few lines to 'const'
+ - line the catch error at line 14 should have been a return statement
+
+ - added general error handler and app.use express.json
